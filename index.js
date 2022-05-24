039e18644c80b3f95b0f83969c1a3dd062ff3999
tanverrana <tanversobur@gmail.com>
2022-05-24 15:20:41 +0600

    data base connected

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygrmxpu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("Database Connected");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get("/", (req, res) => {
     res.send("Running Ts Manufacture Company");
