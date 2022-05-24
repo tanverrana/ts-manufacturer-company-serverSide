d1bcd9b69cc1d20411256f37c4ba55c96f29cc39
tanverrana <tanversobur@gmail.com>
2022-05-24 22:59:36 +0600

    create tools with api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,7 +24,16 @@ async function run() {
             const tools = await cursor.toArray();
             res.send(tools);
 
-        })
+        });
+
+        app.get("/tool/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
+        });
+
+        //review
     }
     finally {
 
