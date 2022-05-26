e6153ddebea5e875b1b886bfa59315da3c1d5355
tanverrana <tanversobur@gmail.com>
2022-05-26 14:26:32 +0600

    create admin role

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
         })
 
         //user create
-        app.put("/user/:email", async (req, res) => {
+        app.put("/user/:email", verifyJWT, async (req, res) => {
             const email = req.params.email;
             const user = req.body;
             const filter = { email: email };
@@ -72,6 +72,16 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
             res.send({ result, token });
+        })
+        app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: "admin" },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
         })
 
         //review
