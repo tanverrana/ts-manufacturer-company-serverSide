408704dc6de0a3e13da7337d2a64941b2b34d0fe
tanverrana <tanversobur@gmail.com>
2022-05-27 12:49:43 +0600

    delete api

--- index.js
+++ index.js
@@ -134,6 +134,14 @@ async function run() {
             return res.send(orders);
 
 
+        });
+
+        //tool delete api
+        app.delete("/tool/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolCollection.deleteOne(query);
+            res.send(result);
         })
 
     }
